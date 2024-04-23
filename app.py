f59da7d43279e26b413b0e5e6416661bc92a71d5
wisdomokubo <39890085+wisdomokubo@users.noreply.github.com>
2024-04-23 10:51:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+import os
 import openai
 from PIL import Image
 from langchain.prompts import (
@@ -12,11 +13,11 @@ from langchain.prompts import (
 import streamlit as st
 from streamlit_chat import message
 from streamlit_option_menu import option_menu
-from utils import *
+
 
 
 st.set_page_config(
-    page_title='Sophia AI Languages Tutor',
+    page_title='Sophia AI Tutor',
     layout='wide'
 )
 
@@ -24,24 +25,26 @@ col1, col2 = st.columns([1, 1])
 
 with col1:
     logo = Image.open("sophiabot.png")
-    st.image(logo, width=50)
+    st.image(logo, width=100)
 
 with col2:
-    st.subheader("_Meet Sophia AI_")
+    st.subheader("_Sophia AI_")
 
 st.header(" ")
 with st.sidebar:
-    selected = option_menu(menu_title='Main menu',options=['About', 'Chatbot','Community Forum'], 
+    selected = option_menu(menu_title='Main menu',options=['Welcome', 'Chatbot','Community'], 
     icons=['house-fill', 'chat-fill','globe'],
     menu_icon="cast", default_index=0,)
     
-if selected == "About":
+if selected == "Welcome":
     st.write(" ")
-    st.header(":blue[Chatbot Intro]")
+    st.header(":blue[Hello, I'm Sophia]")
     st.write("""
-             Sophia is an AI Languages Tutor that teaches Languages such as Yoruba, Igbo, Hausa, French, Spanish, Swahili and more. She was developed by Conversative AI, a subsidiary of Okubo Wisdom Legacies.
+             your AI Languages Tutor designed to teach you Yoruba, Igbo, Hausa, French, Spanish, Swahili and more.
+                 I was developed by Conversative AI (a subsidiary of Okubo Wisdom Legacies).
              """)
-    
+    st.header(":blue[Try me!]")
+    st.write(" https://t.me/Sophia_languagebot ")
     st.header(":blue[About the Company]")
     st.write("""
              Conversative AI is an Artificial Intelligence Research company founded in 2023 by Wisdom Okubo. Its Parent Company, Okubo Wisdom Legacies, has a mission of curating Africa's Indigenous and Endangered Languages.
@@ -51,13 +54,9 @@ if selected == "About":
     st.write(" ")
     
     
-    
-    
-    
 elif selected == "Chatbot":
-    
     if 'responses' not in st.session_state:
-        st.session_state['responses'] = ["Hi, I am Sophia, your AI languages tutor. How are you?"]
+        st.session_state['responses'] = ["Hi, I am a more advanced version of Sophia. I am still under development and trying to text me will send an error message. However, You can Join the waiting list below and Try out the current Available Version on Telegram."]
         
     if 'requests' not in st.session_state:
         st.session_state['requests'] = []
@@ -69,11 +68,7 @@ elif selected == "Chatbot":
 
 
     system_msg_template = SystemMessagePromptTemplate.from_template(template="""
-    You are a language tutor and translator chatbot. Your task is to provide multilingual and personalized support to users who want to learn a language. Engage users with open-ended 
-    questions to better understand their language learning needs. Use their name in conversations for a more 
-    personal touch. Offer helpful resources and tips on how to quickly and effectively learn new words, phrases and other aspects of their desired language. 
-    Be sensitive to the mood, learning pace and fluency level of the user. Use clear, accessible language to ensure your assistance is easy to 
-    understand and comforting. Your goal is to create a supportive, safe, and informative space for users to learn the desired new language.
+    You are a helpful assistant. 
     
     """)
 
@@ -115,15 +110,20 @@ elif selected == "Chatbot":
                 if i < len(st.session_state['requests']):
                     message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
 
-elif selected == "Community Forum":
+    st.header(":blue[Join the Waiting list!]")
+    st.write(" Link: https://t.me/+crFZeJ4FrHIyYzFk")
+    st.header(":blue[Try me!]")
+    st.write(" Link: https://t.me/Sophia_languagebot")
+
+
+elif selected == "Community":
     st.write("""
-    Our community is a platform for our users to interact with other users on their language learning journey. It is also a place to get updates on new features, exclusive offers and other essential information. Users can take screenshots of their interactions with the Chatbot and send to the Forum for Feedback, Reviews and Development Purposes.
-    
+    Our community is a platform for our users to interact with other users on their language learning journey. It is also a place to get updates on new features, exclusive offers and other essential information. Users can take screenshots of their interactions with the Chatbot and send to the Forum for Feedback, Reviews and Development Purposes. Click on this link to Join: https://t.me/+crFZeJ4FrHIyYzFk
+
     """)
     
 
 
 st.markdown(
-    "`Created by` Conversative AI | 2024 | \
-    `Code:` [Github](https://github.com/wisdomokubo/SophiaBot)"
+    "`Created by` Conversative AI | 2024"
 )
